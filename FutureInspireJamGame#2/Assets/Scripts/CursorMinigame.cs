a757f5f81d56866787ccc2ecc13f990a94e36d1b
AJW8 <aaronjw32@gmail.com>
2023-11-04 21:11:07 +1300

    Revert "Merge branch 'Meswif' into Meswif2"
    This reverts commit 2f51972bd76ee761310659b3a54781d845faa596, reversing
    changes made to 6366eb9afca475fe59ee013547f4513a975cb5af.

--- FutureInspireJamGame#2/Assets/Scripts/CursorMinigame.cs
+++ FutureInspireJamGame#2/Assets/Scripts/CursorMinigame.cs
@@ -2,50 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CursorMinigame : Minigame
+public class CursorMinigame : MonoBehaviour
 {
     [SerializeField] private GameObject cursor;
+    [SerializeField] private GameObject zone;
 
-    [SerializeField] private GameObject circlePrefab;
+    [SerializeField] private GameObject cloudPrefab;
 
-    [SerializeField] private Vector2 boundaries;
+    [SerializeField] private Vector2 zoneBoundary;
 
-    [SerializeField] private int rows;
-    [SerializeField] private int columns;
-
-    [SerializeField] private float circleSize;
-    
-    [SerializeField] private int minCirclesInRow;
-    [SerializeField] private int maxCirclesInRow;
-    [SerializeField] private int minCirclesInColumn;
-    [SerializeField] private int maxCirclesInColumn;
-    [SerializeField] private float minAttackTime;
-    [SerializeField] private float maxAttackTime;
-    [SerializeField] private int minAttackCount;
-    [SerializeField] private int maxAttackCount;
-    
-    private int circlesInRow;
-    private int circlesInColumn;
-    private float attackTime;
-    private int attackCount;
+    [SerializeField] private float initialZoneSize;
+    [SerializeField] private float zoneSizeDecrementFactor;
+    [SerializeField] private float initialZoneSpeed;
+    [SerializeField] private float zoneSpeedIncrementFactor;
+    [SerializeField] private float initialMinCloudGeneration;
+    [SerializeField] private float initialMaxCloudGeneration;
+    [SerializeField] private float cloudGenerationDecrementFactor;
 
     private Rigidbody2D cursorRB;
 
-    private Rigidbody2D[] currentCircles;
+    private Vector3 zoneTarget;
 
-    private Vector2[] circlesMoveFrom;
-    private Vector2[] circlesMoveTo;
-
-    private bool rowAttack;
-
-    private int attackIndex;
-    private float currentAttackTime;
+    private bool outOfRange;
 
     // Start is called before the first frame update
     void Start()
     {
         cursorRB = cursor.GetComponent<Rigidbody2D>();
-        cursor.SetActive(false);
+        zone.transform.localScale = Vector3.one * initialZoneSize;
+        zoneTarget = new Vector3(Random.Range(initialZoneSize / 2 - zoneBoundary.x, zoneBoundary.x - initialZoneSize / 2), Random.Range(initialZoneSize / 2 - zoneBoundary.y, zoneBoundary.y - initialZoneSize / 2), 0);
     }
 
     // Update is called once per frame
@@ -56,94 +41,11 @@ public class CursorMinigame : Minigame
 
     void FixedUpdate()
     {
-        if (!playing || win || lose) return;
-        Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        cursorRB.MovePosition(new Vector2(cursorPosition.x < -Mathf.Abs(boundaries.x) ? -Mathf.Abs(boundaries.x) : cursorPosition.x > Mathf.Abs(boundaries.x) ? Mathf.Abs(boundaries.x) : cursorPosition.x, cursorPosition.y < -Mathf.Abs(boundaries.y) ? -Mathf.Abs(boundaries.y) : cursorPosition.y > Mathf.Abs(boundaries.y) ? Mathf.Abs(boundaries.y) : cursorPosition.y));
-        if (intro)
-        {
-            currentAttackTime += Time.deltaTime;
-            if (currentAttackTime >= introTime)
-            {
-                intro = false;
-                Attack();
-            }
-            return;
-        }
-        currentAttackTime += Time.deltaTime;
-        if (currentAttackTime < attackTime)
-        {
-            for (int i = 0; i < currentCircles.Length; i++)
-            {
-                currentCircles[i].MovePosition(Vector2.Lerp(circlesMoveFrom[i], circlesMoveTo[i], currentAttackTime / attackTime));
-                if (!lose && Vector2.Distance(cursorRB.position, currentCircles[i].position) < (circleSize + 0.5f) / 2)
-                {
-                    cursor.SetActive(false);
-                    lose = true;
-                }
-            }
-        }
-        else
-        {
-            if (attackIndex < attackCount) Attack();
-            else
-            {
-                cursor.SetActive(false);
-                foreach (Rigidbody2D c in currentCircles) Destroy(c.gameObject);
-                currentCircles = null;
-                win = true;
-            }
-        }
-    }
-    
-    public override void PlayMinigame(float focusLevel)
-    {
-        playing = true;
-        win = false;
-        lose = false;
-        circlesInRow = (int)(maxCirclesInRow - (maxCirclesInRow - minCirclesInRow) * focusLevel);
-        circlesInColumn = (int)(maxCirclesInColumn - (maxCirclesInColumn - minCirclesInColumn) * focusLevel);
-        attackTime = minAttackTime + (maxAttackTime - minAttackTime) * focusLevel;
-        attackCount = (int)(maxAttackCount - (maxAttackCount - minAttackCount) * focusLevel);
-        intro = true;
-        rowAttack = Random.Range(0, 2) == 0;
-        attackIndex = 0;
-    }
-
-    private void Attack()
-    {
-        attackIndex++;
-        currentAttackTime = 0;
-        rowAttack = !rowAttack;
-        int lines = rowAttack ? rows : columns;
-        int circleCount = rowAttack ? circlesInColumn : circlesInRow;
-        if (currentCircles != null)
-        {
-            foreach (Rigidbody2D c in currentCircles) Destroy(c.gameObject);
-        }
-        currentCircles = new Rigidbody2D[circleCount];
-        circlesMoveFrom = new Vector2[circleCount];
-        circlesMoveTo = new Vector2[circleCount];
-        bool[] index = new bool[lines];
-        for (int i = 0; i < circleCount; i++)
-        {
-            int line;
-            do line = Random.Range(0, lines);
-            while (index[line]);
-            index[line] = true;
-            currentCircles[i] = Instantiate(circlePrefab, transform.position, transform.rotation).GetComponent<Rigidbody2D>();
-            currentCircles[i].transform.localScale = (Vector3.right + Vector3.up) * circleSize + Vector3.forward;
-            bool startNegative = Random.Range(0, 1) == 0;
-            if (rowAttack)
-            {
-                circlesMoveFrom[i] = new Vector2(startNegative? -boundaries.x - 0.5f : boundaries.x + 0.5f, boundaries.y * (line + 0.5f - rows / 2f) * 2f / rows);
-                circlesMoveTo[i] = new Vector2(startNegative ? boundaries.x + 0.5f : -boundaries.x - 0.5f, boundaries.y * (line + 0.5f - rows / 2f) * 2f / rows);
-            }
-            else
-            {
-                circlesMoveFrom[i] = new Vector2(boundaries.x * (line + 0.5f - columns / 2f) * 2f / columns, startNegative ? -boundaries.y - 0.5f : boundaries.y + 0.5f);
-                circlesMoveTo[i] = new Vector2(boundaries.x * (line + 0.5f - columns / 2f) * 2f / columns, startNegative ? boundaries.y + 0.5f : -boundaries.y - 0.5f);
-            }
-            currentCircles[i].MovePosition(circlesMoveFrom[i]);
-        }
+        if (outOfRange) return;
+        cursorRB.MovePosition(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+        zone.transform.position = Vector3.MoveTowards(zone.transform.position, zoneTarget, initialZoneSpeed * Time.deltaTime);
+        Vector2 zonePosition = new Vector2(zone.transform.position.x, zone.transform.position.y);
+        if (Vector2.Distance(zonePosition, new Vector2(zoneTarget.x, zoneTarget.y)) < 0.1f) zoneTarget = new Vector3(Random.Range(initialZoneSize / 2 - zoneBoundary.x, zoneBoundary.x - initialZoneSize / 2), Random.Range(initialZoneSize / 2 - zoneBoundary.y, zoneBoundary.y - initialZoneSize / 2), 0);
+        outOfRange = Vector2.Distance(cursorRB.position, zonePosition) > initialZoneSize / 2;
     }
 }
--- FutureInspireJamGame#2/Assets/Scripts/GameManagerScript.cs
+++ FutureInspireJamGame#2/Assets/Scripts/GameManagerScript.cs
@@ -1,28 +1,111 @@
+using Capabilities;
+using Dialogue;
+using MiniGames;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class GameManagerScript : MonoBehaviour
+namespace Managers
 {
-    public int Minute;
-    public int Hour;
-
-    public int KnowledgePoints;
-
-    public AudioSource MainTrack;
-
-    public float FocusLevel;
-
-    // Start is called before the first frame update
-    void Start()
+    public enum Days
     {
-        
+        Monday,
+        Tesuday,
+        Wedndesday,
+        Thursday,
+        Friday
+    }
+    public enum GameState
+    {
+        Narration,
+        Walking,
+        Sleeping,
+        MiniGame,
+        Eating,
+        Paused
     }
 
-    // Update is called once per frame
-    void Update()
+    public class GameManagerScript : MonoBehaviour
     {
-        Minute = TimeManagerScript.Minute;
-        Hour = TimeManagerScript.Hour;
+        [SerializeField] private GameObject playerObject;
+        [SerializeField] private List<GameEventSO> eventList;
+        [SerializeField] private NodeParser narrator;
+
+        public int Minute;
+        public int Hour;
+        
+
+        public int KnowledgePoints;
+
+        public AudioSource MainTrack;
+        private Move playerMove;
+        private Interact playerInteract;
+        private GameState gameState;
+        private Days dayOfTheWeek;
+
+        private int eventIndex;
+        private void Awake()
+        {
+            eventIndex = 0;
+            dayOfTheWeek = Days.Monday;
+            
+            playerMove = playerObject.GetComponent<Move>();
+            playerInteract = playerObject.GetComponent<Interact>();
+        }
+        private void OnEnable()
+        {
+            RestartPlayerInput();
+            NodeParser.OnNarrationEnded += NodeParser_OnNarrationEnded;
+            MiniGameManager.OnMiniGameEnded += MiniGameManager_OnMiniGameEnded;
+        }
+
+        private void MiniGameManager_OnMiniGameEnded()
+        {
+            Debug.Log("MiniGame Ended");
+            RestartPlayerInput();
+        }
+
+        private void NodeParser_OnNarrationEnded()
+        {
+            Debug.Log("Narration Ended");
+            RestartPlayerInput();
+        }
+
+        private void OnDisable()
+        {
+            NodeParser.OnNarrationEnded -= NodeParser_OnNarrationEnded;
+            MiniGameManager.OnMiniGameEnded -= MiniGameManager_OnMiniGameEnded;
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            Minute = TimeManagerScript.Minute;
+            Hour = TimeManagerScript.Hour;
+            if (eventIndex > eventList.Count) return;
+            if (dayOfTheWeek == eventList[eventIndex].day && Hour == eventList[eventIndex].hour)
+            {
+                narrator.SetDialogueGraph(eventList[eventIndex].dialogueGraph);
+                narrator.BeginDialogue();
+                StopPlayerInput();
+                TimeManagerScript.timePaused = true;
+                eventIndex++;
+            }
+        }
+        public void StopPlayerInput()
+        {
+            playerMove.StopMovement();
+            playerInteract.StopInteraction();
+            TimeManagerScript.timePaused = true;
+        }
+        public void RestartPlayerInput()
+        {
+            playerMove.Restartmovement();
+            playerInteract.RestartInteraction();
+            TimeManagerScript.timePaused = false;
+        }
     }
 }
+
